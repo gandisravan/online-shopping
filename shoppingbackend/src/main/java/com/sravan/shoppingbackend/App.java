603f286664e5ad1d743643bf99ce0b9b77154351
sravan <gandi.sravan.kmr@gmail.com>
2019-12-16 12:59:33 +0800

    my project setup completed

---
+++ shoppingbackend/src/main/java/com/sravan/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.sravan.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
